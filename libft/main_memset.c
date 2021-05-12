a1ef4b8c8329593863a9927e7a68142f4aec287b
Beh Guan Hong <80773170+gh-beh@users.noreply.github.com>
2021-05-12 15:00:10 +0800

    Update main_memset.c

--- libft/main_memset.c
+++ libft/main_memset.c
@@ -15,7 +15,7 @@ int	main(void)
 	// Test 2 : Boundary values
 	printf("C memset:\t%s\n", memset(s1, d, (0)));
 	printf("ft_memset:\t%s\n", ft_memset(s2, d, (0)));
-	// Test 3 : Unsigned char
+	// Test 3 : Unsigned char, run with cat -e
 	printf("C memset:\t%s\n", memset(s1, 129, 3));
 	printf("ft_memset:\t%s\n", ft_memset(s2, 129, 3));
 	// Test 4 : Functionality
