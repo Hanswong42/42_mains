221e89e014e7a6379062a9d1c8bfa430ad71cdad
Beh Guan Hong <80773170+gh-beh@users.noreply.github.com>
2021-05-12 14:58:04 +0800

    Update main_memset.c
    silence gcc warning

--- libft/main_memset.c
+++ libft/main_memset.c
@@ -13,8 +13,8 @@ int	main(void)
 	printf("C memset:\t%s\n", memset(s1, c, 3));
 	printf("ft_memset:\t%s\n", ft_memset(s2, c, 3));
 	// Test 2 : Boundary values
-	printf("C memset:\t%s\n", memset(s1, d, 0));
-	printf("ft_memset:\t%s\n", ft_memset(s2, d, 0));
+	printf("C memset:\t%s\n", memset(s1, d, (0)));
+	printf("ft_memset:\t%s\n", ft_memset(s2, d, (0)));
 	// Test 3 : Unsigned char
 	printf("C memset:\t%s\n", memset(s1, 129, 3));
 	printf("ft_memset:\t%s\n", ft_memset(s2, 129, 3));
